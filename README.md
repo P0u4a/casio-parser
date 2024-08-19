a7b1862c2d502e70efcfe6927b70cdacfa6743e3
P0u4a <pouyamiria@gmail.com>
2024-08-20 02:00:07 +0930

    Take input from command line

---
+++ README.md
@@ -0,0 +1,7 @@
+# Casio Model Code Parser ⌚
+
+# Running
+
+1. Build the project with `cargo build`
+2. Test with `cargo test`
+3. Run with `cargo run -- <watch-type>`
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 mod mappings;
 mod parser;
+use std::env;
 
 fn main() {
-    println!("Hello, world!");
+    // TODO print model info in a more user-friendly format
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("Please pass a watch code like this: cargo run -- <watch-code>");
+    }
+
+    let (watch_type, tokens) = parser::tokensier::tokeniser(&args[1]);
+
+    let model_info = parser::parser::parse_record(&watch_type, tokens);
+    if let Some(value) = model_info {
+        println!("{:?}", value);
+    } else {
+        eprintln!("Error parsing watch code");
+    }
 }
